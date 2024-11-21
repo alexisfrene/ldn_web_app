db44d825e5533647148d63fe813ceaaa9454a06b
alexisfrene <alexisfrene@gmail.com>
2024-11-21 13:21:03 -0300

    fix not open sidebar

--- src/presentation/components/common/Layout/index.tsx
+++ src/presentation/components/common/Layout/index.tsx
@@ -11,40 +11,50 @@ import {
 } from '@components';
 import { AppSidebar } from '../AppSidebar';
 
-interface Props {
+interface LayoutProps {
   children: ReactNode;
 }
 
-export const Layout: React.FC<Props> = ({ children }) => {
-  const insertAvatar = useSessionStore((state) => state.insertAvatar);
-  const avatar = useSessionStore((state) => state.avatar);
-  const session_token = useSessionStore((state) => state.session_token);
-  const getAvatarImage = async () => {
-    const res = await getUrlAvatar();
-    insertAvatar(res);
-  };
+const useAvatar = () => {
+  const { session_token, avatar, insertAvatar } = useSessionStore((state) => ({
+    session_token: state.session_token,
+    avatar: state.avatar,
+    insertAvatar: state.insertAvatar,
+  }));
 
   useEffect(() => {
-    if (session_token && !avatar) {
-      getAvatarImage();
-    }
-  }, []);
+    const fetchAvatar = async () => {
+      if (session_token && !avatar) {
+        const avatarUrl = await getUrlAvatar();
+        insertAvatar(avatarUrl);
+      }
+    };
+
+    fetchAvatar();
+  }, [session_token, avatar, insertAvatar]);
+
+  return { avatar, session_token };
+};
+
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const { avatar, session_token } = useAvatar();
+
   return (
-    <SidebarProvider open={!!session_token}>
-      <AppSidebar />
+    <SidebarProvider>
+      {session_token && <AppSidebar />}
       <main className="flex-1">
-        <div className="flex items-center justify-between bg-gradient-to-t from-amber-200 to-amber-400 p-3 dark:from-slate-700 dark:to-slate-900 dark:text-slate-200">
+        <header className="flex items-center justify-between bg-gradient-to-t from-amber-200 to-amber-400 p-3 dark:from-slate-700 dark:to-slate-900 dark:text-slate-200">
           {session_token && <SidebarTrigger />}
           <div className="flex items-center">
             <ModeToggle />
             {session_token && (
               <Avatar className="mx-5 my-2">
-                <AvatarImage src={avatar} alt="@shadcn" />
+                <AvatarImage src={avatar} alt="User Avatar" />
                 <AvatarFallback>CN</AvatarFallback>
               </Avatar>
             )}
           </div>
-        </div>
+        </header>
         {children}
       </main>
     </SidebarProvider>

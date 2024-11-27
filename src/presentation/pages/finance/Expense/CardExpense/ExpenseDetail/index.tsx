096b6f461b2616ad99a37f11166b94075ef16202
alexisfrene <alexisfrene@gmail.com>
2024-11-27 10:53:37 -0300

    fix : add skeleton in ExpenseDetail

--- src/presentation/pages/finance/Expense/CardExpense/ExpenseDetail/index.tsx
+++ src/presentation/pages/finance/Expense/CardExpense/ExpenseDetail/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 import {
   Dialog,
   DialogContent,
@@ -11,8 +13,6 @@ import {
   Skeleton,
 } from '@components';
 import { getExpenseById } from '@services';
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
 
 interface Props {
   children: React.ReactNode;
@@ -30,9 +30,6 @@ export const ExpenseDetail: React.FC<Props> = ({
     queryFn: () => getExpenseById({ expense_id }),
   });
 
-  if (expenses.isPending) {
-    return <Skeleton className="h-[65vh] w-[85vw]" />;
-  }
   if (expenses.error) return 'An error has occurred: ';
   return (
     <Dialog>
@@ -40,23 +37,29 @@ export const ExpenseDetail: React.FC<Props> = ({
         {children}
       </DialogTrigger>
       <DialogContent className="max-w-3xl">
-        <DialogHeader>
-          <DialogTitle>
-            Detalles sobre la categoría de gastos llamada :{name}
-          </DialogTitle>
-          <DialogDescription>
-            En este apartado podrá ver , todos los movimientos que se ejecutaron
-            en el mismo :
-          </DialogDescription>
-        </DialogHeader>
-        <Label>Nombre : {expenses.data.name}</Label>
-        <Label>Descripción : {expenses.data.description}</Label>
-        <ScrollArea className="h-96 px-3">
-          <MovementList
-            isPending={expenses.isPending}
-            movements={expenses.data.movements}
-          />
-        </ScrollArea>
+        {expenses.isPending ? (
+          <Skeleton />
+        ) : (
+          <>
+            <DialogHeader>
+              <DialogTitle>
+                Detalles sobre la categoría de gastos llamada :{name}
+              </DialogTitle>
+              <DialogDescription>
+                En este apartado podrá ver , todos los movimientos que se
+                ejecutaron en el mismo :
+              </DialogDescription>
+            </DialogHeader>
+            <Label>Nombre : {expenses.data.name}</Label>
+            <Label>Descripción : {expenses.data.description}</Label>
+            <ScrollArea className="h-96 px-3">
+              <MovementList
+                isPending={expenses.isPending}
+                movements={expenses.data.movements}
+              />
+            </ScrollArea>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );

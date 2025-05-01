3cf1213bd23b211f693601b32212051f7b433762
Alexis <alexisfrene@gmail.com>
2025-05-01 02:33:58 -0300

    feat: refactor user forms to use Formik for improved form handling and structure

--- src/presentation/pages/filing/index.tsx
+++ src/presentation/pages/filing/index.tsx
@@ -1,50 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Opulento, Velustro } from 'uvcanvas';
-import { useFormik } from 'formik';
 import { useSessionStore } from '@global';
 import {
   BorderBeam,
-  Button,
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-  Input,
-  Label,
-  LoadingButton,
   Particles,
   useTheme,
 } from '@components';
-import { loginUser } from '@services';
+import { LoginUserForm } from '@forms';
 
 const Filing: React.FC = () => {
   const navigate = useNavigate();
   const sessionToken = useSessionStore((state) => state.session_token);
   const { theme } = useTheme();
   const [color, setColor] = useState('#ffffff');
-
-  const insertSessionToken = useSessionStore(
-    (state) => state.insertSessionToken,
-  );
-  const formik = useFormik({
-    initialValues: {
-      email_or_user: '',
-      password: '',
-    },
-
-    onSubmit: async (values, formikHelpers) => {
-      const res = await loginUser(values);
-
-      if (res?.data.session_token) {
-        insertSessionToken(res?.data.session_token);
-        return setTimeout(() => navigate('/app/finance'), 200);
-      } else {
-        formikHelpers.resetForm();
-      }
-    },
-  });
   const [hasAnimated, setHasAnimated] = useState(false);
 
   useEffect(() => {
@@ -94,37 +68,7 @@ const Filing: React.FC = () => {
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <form onSubmit={formik.handleSubmit}>
-                <Label>Email/Username</Label>
-                <Input
-                  id="email_or_user"
-                  name="email_or_user"
-                  placeholder="Ej : juanperez003"
-                  value={formik.values.email_or_user}
-                  onChange={formik.handleChange}
-                />
-                <Label>Contraseña</Label>
-                <Input
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={formik.values.password}
-                  placeholder="********"
-                  onChange={formik.handleChange}
-                />
-                <div className="mt-6 flex justify-between">
-                  <Button
-                    variant="outline"
-                    type="button"
-                    onClick={() => navigate('/signup')}
-                  >
-                    Register
-                  </Button>
-                  <LoadingButton type="submit" loading={formik.isSubmitting}>
-                    Iniciar sesión
-                  </LoadingButton>
-                </div>
-              </form>
+              <LoginUserForm />
             </CardContent>
             <BorderBeam
               duration={8}
